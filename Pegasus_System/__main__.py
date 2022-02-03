8a12367d53d67ac3254f64c7336563a7ec532b36
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-02-03 08:23:48 +0530

    Update __main__.py

--- Pegasus_System/__main__.py
+++ Pegasus_System/__main__.py
@@ -6,6 +6,7 @@ from Pegasus_System import (
     ENFORCERS,
     Pegasus_logs,
 )
+from telegram import InlineKeyboardButton,InlineKeyboardMarkup
 from Pegasus_System.strings import on_string
 import logging
 import importlib
@@ -110,7 +111,16 @@ async def main():
             msg += f"\n**{plugin}**\n\n`{FAILED_TO_LOAD[plugin]}`"
         await System.send_message(Pegasus_logs, msg)
     else:
-        await System.send_file(Pegasus_logs,file="http://telegra.ph/file/ff4816777018617806e36.mp4",caption="System Is Alive!")
+        await System.send_file(Pegasus_logs,file="http://telegra.ph/file/ff4816777018617806e36.mp4",caption="System Is Alive!",reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                "Support", url="https://t.me/pegasus_support"),
+                            InlineKeyboardButton(
+                                "Updates", url="https://t.me/PegasusSystem")
+                        ],
+                    ]
+                )
     await System.run_until_disconnected()
 
 
