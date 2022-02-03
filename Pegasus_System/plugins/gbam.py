507cbff6f96d1b82dfcb9d450f93545d64a93ed2
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2022-02-03 10:31:23 +0530

    Update gbam.py

--- Pegasus_System/plugins/gbam.py
+++ Pegasus_System/plugins/gbam.py
@@ -1,1 +1,16 @@
+from Pegasus_System import system_cmd, System
 
+GBAM_MSG = ( "Fucking with bots",
+             "play with gey's",
+             "abuseing with dick",)
+
+
+    @System.on(system_cmd(pattern=r"gbam"))
+async def (event) -> None:
+    replied_user = await event.get_reply_message()
+    
+    await System.send_message(
+               event.chat_id,random.choice(GBAM_MSG))
+        
+        
+        
